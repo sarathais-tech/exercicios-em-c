5da2f6a3eacc543e9be1c60b634021a9ac5fd287
sasacatt <159670375+sasacatt@users.noreply.github.com>
2025-11-07 17:47:01 -0300

    Add files via upload

---
+++ 03-loops/validar_idade_do_while_if_else.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+
+
+    do {
+        printf("Digite a idade (de 1 a 123): ");
+        scanf("%d", &idade);
+
+
+        if (idade < 1 || idade > 123) {
+            printf("Idade invalida. Tente novamente.\n");
+        } else {
+            printf("Idade valida!\n");
+        }
+    } while (idade < 1 || idade > 123);
+
+    return 0;
+}
+
