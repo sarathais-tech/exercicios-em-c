fda847410e96aeb95888092a2e9ed434ab21b481
Sara Thais <sarathais.tech@gmail.com>
2026-01-11 16:28:02 -0300

    Add files via upload

---
+++ 02-operacoes-matematicas/multiplicacao.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	
+	int numero1, numero2, resultado;
+	
+	printf("Entre com o primeriro numero:");
+	scanf("%d", & numero1);
+	
+	printf("Entre com o segundo numero:");
+	scanf("%d", & numero2);
+	
+	resultado = numero1 * numero2;
+	
+	printf("A multiplicacao e:%d\n", resultado);
+	
+	
+	
+	
+ return 0;	
+}
+
