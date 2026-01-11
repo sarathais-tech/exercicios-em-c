a76113d830a3fcb78c6f2ec65b469c0e14ba4a37
Sara Thais <sarathais.tech@gmail.com>
2026-01-11 16:27:35 -0300

    Add files via upload

---
+++ 02-operacoes-matematicas/divisao.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	int num1, num2, divisao;
+	
+	printf("digite o primeiro numero:");
+	scanf("%d", & num1);
+	
+	printf("digite o segundo numero:");
+	scanf("%d", & num2);
+	
+	divisao = num1 / num2;
+	
+	printf("o resultado e: %d\n", divisao);
+	
+	
+	return 0;
+	
+}
